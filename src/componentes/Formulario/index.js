f6ac10755bca93228f941c22e619d11f224c3485
matteorudas <matteorudas@gmail.com>
2025-02-02 23:54:19 -0300

    Agregando el boton y entendiendo los eventos

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,20 +1,60 @@
+import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
+import { useState } from 'react';
 import './Formulario.css';
 
 const Formulario = () => {  
 
     const times = ['Desenvolvimento', 'Marketing', 'Comercial', 'Financeiro', 'Administrativo'];
 
+    const [nome, setNome] = useState('');
+    const [cargo, setCargo] = useState('');
+    const [emial, setEmail] = useState('');
+    const [imagem, setImagem] = useState('');
+    const [time, setTime] = useState('');
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault();
+        console.log('Salvando... => ' + nome + ' - ' + cargo + ' - ' + emial + ' - ' + imagem + ' - ' + time);
+    }
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Prencha os dados do colaborador</h2>
-                <CampoTexto label="Nombre:" placeholder="Digite seu nome..."/> 
-                <CampoTexto label="Cargo:"  placeholder="Digite seu cargo..."/>
-                <CampoTexto label="Email:"  placeholder="Digite seu Email..."/>
-                <CampoTexto label="Imagem:" placeholder="Informe a URL da sua imagem..."/> 
-                <ListaSuspensa label="Times:" itens={times}/>      
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Nome:" 
+                    placeholder="Digite seu nome..."
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}/> 
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Cargo:"  
+                    placeholder="Digite seu cargo..."
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}/>
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Email:"  
+                    placeholder="Digite seu Email..."
+                    valor={emial}
+                    aoAlterado={valor => setEmail(valor)}/>
+                <CampoTexto 
+                    label="Imagem:" 
+                    placeholder="Informe a URL da sua imagem..."
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}/> 
+                <ListaSuspensa 
+                    obrigatorio={true} 
+                    label="Times:" 
+                    itens={times}
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)}/>      
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
